5a12c0c78281a4c442b13ad8feec6e50aec16e99
EdPaul1 <saidarajab101@gmail.com>
2023-05-15 22:16:30 +0300

    Operators in Ruby

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,54 @@
 def admin_login(username, password)
   # your code here
+  if (username == "admin" || username == "ADMIN") && password == "12345"
+    "Access granted"
+  else
+    "Access denied"
 end
 
 def hows_the_weather(temperature)
   # your code here
+    let response;
+  if temperature < 40 
+    response = "brisk";
+  else if temperature >= 40 && temperature <= 65 
+    response = "a little chilly";
+  else if temperature > 85 
+    response = "too dang hot";
+  else 
+    response = "perfect";
+  
+  return "It's #{response} out there!";
 end
 
 def fizzbuzz(num)
-  # your code here
+  if num % 3 == 0 && num % 5 == 0
+    "FizzBuzz"
+  elsif num % 3 == 0
+    "Fizz"
+  elsif num % 5 == 0
+    "Buzz"
+  else
+    num
+  end
 end
 
+
 def calculator(operation, num1, num2)
-  # your code here
+  result = case operation
+  when '+'
+    num1 + num2
+  when '-'
+    num1 - num2
+  when '*'
+    num1 * num2
+  when '/'
+    num1 / num2
+  else
+    puts "Invalid operation!"
+    nil
+  end
+  return result
 end
+
 
