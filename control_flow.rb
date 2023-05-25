3e7115be25cc47de01cf3c7db5eb4c6bd64d045b
EdPaul1 <saidarajab101@gmail.com>
2023-05-25 16:40:45 +0300

    conditional flow statements

--- control_flow.rb
+++ control_flow.rb
@@ -1,54 +1,51 @@
 def admin_login(username, password)
-  # your code here
-  if (username == "admin" || username == "ADMIN") && password == "12345"
-    "Access granted"
+  username = username.downcase
+
+  if username == "admin" && password == "12345"
+    return "Access granted"
   else
-    "Access denied"
+    return "Access denied"
+  end
 end
+
 
 def hows_the_weather(temperature)
-  # your code here
-    let response;
-  if temperature < 40 
-    response = "brisk";
-  else if temperature >= 40 && temperature <= 65 
-    response = "a little chilly";
-  else if temperature > 85 
-    response = "too dang hot";
-  else 
-    response = "perfect";
-  
-  return "It's #{response} out there!";
-end
-
-def fizzbuzz(num)
-  if num % 3 == 0 && num % 5 == 0
-    "FizzBuzz"
-  elsif num % 3 == 0
-    "Fizz"
-  elsif num % 5 == 0
-    "Buzz"
+  if temperature < 40
+    return "It's brisk out there!"
+  elsif temperature >= 40 && temperature <= 65
+    return "It's a little chilly out there!"
+  elsif temperature > 85
+    return "It's too dang hot out there!"
   else
-    num
+    return "It's perfect out there!"
   end
 end
 
+
+def fizzbuzz(number)
+  if number % 3 == 0 && number % 5 == 0
+    return "FizzBuzz"
+  elsif number % 3 == 0
+    return "Fizz"
+  elsif number % 5 == 0
+    return "Buzz"
+  else
+    return number
+  end
+end
 
 def calculator(operation, num1, num2)
-  result = case operation
-  when '+'
-    num1 + num2
-  when '-'
-    num1 - num2
-  when '*'
-    num1 * num2
-  when '/'
-    num1 / num2
+  case operation
+  when "+"
+    return num1 + num2
+  when "-"
+    return num1 - num2
+  when "*"
+    return num1 * num2
+  when "/"
+    return num1 / num2
   else
     puts "Invalid operation!"
-    nil
+    return nil
   end
-  return result
 end
-
-
